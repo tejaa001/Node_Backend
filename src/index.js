eee7a205f77825c6b553d77581cc2573e8cc2151
Tejas <tejasbhalerao85@gmail.com>
2025-06-14 11:47:53 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import dotenv from "dotenv"
-import mongoose from "mongoose";
 import connectDB from "./db/db.js";
+import express from "express"
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+const app = express()
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,  ()=>{
+        console.log(`Server is Running at port : ${process.env.PORT}`);
+        app.on("error", (error)=>{
+            console.log("ERROR",error);
+            throw error 
+        })
+    })
+})
+.catch((err)=>{
+    console.log("DB Connection is failed",err);  
+})
 
 
 
---
+++ src/utils/ApiErrors.js
@@ -0,0 +1,16 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Someting Went Wrong",
+        errors = [],
+        stack =""
+
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = this.errors
+    }
+}
